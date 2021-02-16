5bb8fa1c784637412a9e6ede6e01a3ebc7b3952e
dashlykova <daria.lykova@gmail.com>
2021-02-16 22:12:39 +0100

    Configuration for GoogleLogin function

--- ProjectEdnaApp/ProjectEdnaWebb/Program.cs
+++ ProjectEdnaApp/ProjectEdnaWebb/Program.cs
@@ -1,26 +1,27 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace ProjectEdnaWebb
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
+namespace ProjectEdnaWebb {
+  public class Program {
+    public static void Main (string[] args) {
+      var host = new HostBuilder ().ConfigureAppConfiguration ((hostContext, builder) => {
+        if (hostContext.HostingEnvironment.IsDevelopment ()) {
+          builder.AddUserSecrets<Program> ();
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      }).Build ();
+      host.Run();
     }
+
+    public static IHostBuilder CreateHostBuilder (string[] args) =>
+      Host.CreateDefaultBuilder (args)
+      .ConfigureWebHostDefaults (webBuilder => {
+        webBuilder.UseStartup<Startup> ();
+      });
+  }
 }
